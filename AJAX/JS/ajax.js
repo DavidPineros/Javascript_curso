781e3ccdd840fc710951d9858b55383829aff73e
David Pinto <davidpinerospinto@gmail.com>
2022-04-21 09:03:45 -0500

    AJAX con libreria externa: Axios

--- AJAX/JS/ajax.js
+++ AJAX/JS/ajax.js
@@ -76,7 +76,7 @@
       let res = await fetch("https://jsonplaceholder.typicode.com/users"),
         json = await res.json();
 
-      console.log(res, json);
+      //console.log(res, json);
 
       //if (!res.ok) throw new Error("Ocurrio un Error al solicitar los Datos");
       if (!res.ok) throw { status: res.status, statusText: res.statusText };
@@ -92,7 +92,67 @@
       let message = err.statusText || "Ocurrio un error";
       $fetchAsync.innerHTML = `Error ${err.status}: ${message}`;
     } finally {
-      console.log("Esto se ejecutara independientemente del try catch");
+      //console.log("Esto se ejecutara independientemente del try catch");
+    }
+  }
+
+  getData();
+})();
+
+//AJAX con libreria externa: Axios
+
+(() => {
+  const $axios = document.getElementById("axios"),
+    $fragment = document.createDocumentFragment();
+
+  axios
+    .get("https://jsonplaceholder.typicode.com/users")
+    .then((res) => {
+      // console.log(res);
+      res.data.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+        $fragment.appendChild($li);
+      });
+      $axios.appendChild($fragment);
+    })
+    .catch((err) => {
+      // console.log(err.response);
+      let message = err.response.statusText || "Ocurrio un error";
+      $axios.innerHTML = `Error ${err.response.status}: ${message}`;
+    })
+    .finally(() => {
+      //console.log("Esto se ejecutara independientemente del resultado de Axios");
+    });
+})();
+
+//AJAX con libreria externa: Axios con Async-Await
+
+(() => {
+  const $axiosAsync = document.getElementById("axios-async"),
+    $fragment = document.createDocumentFragment();
+
+  axios.get("https://jsonplaceholder.typicode.com/users");
+
+  async function getData() {
+    try {
+      let res = await axios.get("https://jsonplaceholder.typicode.com/users"),
+        json = await res.data;
+
+      console.log(res, json);
+
+      json.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+        $fragment.appendChild($li);
+      });
+      $axiosAsync.appendChild($fragment);
+    } catch (err) {
+      console.log(err.response);
+      let message = err.response.statusText || "Ocurrio un error";
+      $axiosAsync.innerHTML = `Error ${err.response.status}: ${message}`;
+    } finally {
+      console.log("Esto se ejecutara independientemente de lo que suceda");
     }
   }
 
